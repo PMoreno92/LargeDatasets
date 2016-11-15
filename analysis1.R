3deb2e44dc3c4d82c51663bc972cd19541fa493b
Pablo Moreno Garcia <pablo.paul.paolo@gmail.com>
2016-11-15 15:34:36 -0500

    Code to obain the data
    Chages to classify the individuals

--- analysis1.R
+++ analysis1.R
@@ -1,3 +1,11 @@
 get_data <- function(){
   data <- read.csv("surveys.cvs")
 }
+get_size_class <- function(weight){
+  if (weight>50){
+    size_class = "large"
+  }else{
+    size_class= "small"
+  }
+  return(size_class)
+}
